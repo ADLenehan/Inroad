d12c52ae08cfaed2a677887e21867ac3a1b46b22
ADLenehan <andrew.lenehan@gmail.com>
2019-08-16 10:24:15 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,7 +34,7 @@ window.onload = async () => {
 
   // NEW - check for the code and state parameters
   const query = window.location.search;
-  if (query.includes("code")) {
+  if (query.includes("code=") && query.includes("state=")) {
 
     // Process the login state
     await auth0.handleRedirectCallback();
