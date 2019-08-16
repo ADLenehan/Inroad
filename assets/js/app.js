10f3dea589d4a35aee926818f1b625f93875e7a6
ADLenehan <andrew.lenehan@gmail.com>
2019-08-15 22:24:13 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,82 +1,44 @@
-let auth0 = null;
-const fetchAuthConfig = () => fetch("/auth_config.json");
+var Auth = (function() {
 
-var lock = new Auth0Lock('NHAcMO0QsAek2ftHsriSFGRi6RIr8QTO','inroad.auth0.com', options);
+  var wm = new WeakMap();
+  var privateStore = {};
+  var lock;
 
-var options = {
-   auth: {
-   redirectUrl: 'https://syllabus.inroad.co/',
-   responseType: 'code',
-   params: {state: 'foo'}
-  }
-};
-
-const configureClient = async () => {
-  const response = await fetchAuthConfig();
-  const config = await response.json();
-
-  auth0 = await createAuth0Client({
-    domain: config.domain,
-    client_id: config.clientId
-  });
-};
-
-window.onload = async () => {
-  await configureClient();
-
-  // NEW - update the UI state
-  updateUI();
-  
-    const isAuthenticated = await auth0.isAuthenticated();
-
-  if (isAuthenticated) {
-    return;
-  }
-
-  // NEW - check for the code and state parameters
-  const query = window.location.search;
-  if (query.includes("code=") && query.includes("state=")) {
-
-    // Process the login state
-    await auth0.handleRedirectCallback();
-    
-    updateUI();
-
-    // Use replaceState to redirect the user away and remove the querystring parameters
-    window.history.replaceState({}, document.title, "/");
-  }
-};
-
-
-// NEW
-const updateUI = async () => {
-  const isAuthenticated = await auth0.isAuthenticated();
-
-  document.getElementById("btn-logout").disabled = !isAuthenticated;
-  document.getElementById("btn-login").disabled = isAuthenticated;
-  
-    if (isAuthenticated) {
-    document.getElementById("gated-content").classList.remove("hidden");
-
-    document.getElementById(
-      "ipt-access-token"
-    ).innerHTML = await auth0.getTokenSilently();
-
-    document.getElementById("ipt-user-profile").innerHTML = JSON.stringify(
-      await auth0.getUser()
+  function Auth() {
+    this.lock = new Auth0Lock(
+      'NHAcMO0QsAek2ftHsriSFGRi6RIr8QTO',
+      'inroad.auth0.com'
     );
-
-  } else {
-    document.getElementById("gated-content").classList.add("hidden");
+    wm.set(privateStore, {
+      appName: "example"
+    });
   }
-};
 
-const login = async () => {
-  lock.show();
-};
+  Auth.prototype.getProfile = function() {
+    return wm.get(privateStore).profile;
+  };
 
-const logout = () => {
-  auth0.logout({
-    returnTo: window.location.origin
-  });
-};
+  Auth.prototype.authn = function() {
+    // Listening for the authenticated event
+    this.lock.on("authenticated", function(authResult) {
+      // Use the token in authResult to getUserInfo() and save it if necessary
+      this.getUserInfo(authResult.accessToken, function(error, profile) {
+        if (error) {
+          // Handle error
+          return;
+        }
+
+        //we recommend not storing Access Tokens unless absolutely necessary
+        wm.set(privateStore, {
+          accessToken: authResult.accessToken
+        });
+
+        wm.set(privateStore, {
+          profile: profile
+        });
+
+      });
+    });
+  };
+  return Auth;
+}());
